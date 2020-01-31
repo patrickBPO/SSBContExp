9d36d724ee9f6f2fe690d2b65b6355baff0b327d
Patrick Castillo <knightiii@hotmail.com>
2020-01-31 11:41:43 -0600

    Commit small mod to Remain on the current record edited

--- SSBContExp/Forms/DXCompany.vb
+++ SSBContExp/Forms/DXCompany.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Globalization
 Imports DevExpress.Data
+Imports DevExpress.XtraEditors
 Imports DevExpress.XtraGrid.Views.Base
 Imports Microsoft.Office.Interop
 Public Class DXCompany
@@ -20,10 +21,21 @@ Public Class DXCompany
     End Sub
     '{-- DevExpress Elegant way - Utilize Grid Control Gridview with Control Navigator Physically update Database Rows --}
     Private Sub GridView1_RowUpdated(sender As Object, e As RowObjectEventArgs) Handles GridView1.RowUpdated
+        Dim Idx As Int16 = GridView1.FocusedRowHandle
         Me.Ss_companyTableAdapter1.Update(Me.CompMaintDS.ss_company)
+        '- Used Below coed to avoid concurrency errors if Updating right after adding new row
+        Me.Ss_companyTableAdapter1.Fill(Me.CompMaintDS.ss_company)
+        GridView1.MoveBy(Idx) '- Move back to Original focused row
+
     End Sub
 
     Private Sub GridView1_RowDeleted(sender As Object, e As RowDeletedEventArgs) Handles GridView1.RowDeleted
         Me.Ss_companyTableAdapter1.Adapter.Update(CompMaintDS)
     End Sub
+
+    'Private Sub CNav_ButtonClick(sender As Object, e As NavigatorButtonClickEventArgs) Handles CNav.ButtonClick
+    '    If e.Button Is CNav.Buttons.EndEdit Then
+    '        GridView1.MoveLast()
+    '    End If
+    'End Sub
 End Class
